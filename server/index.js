3ed55fb1e895136ccd4c238a6ccd5d2afb306d88
inatbalthazar <inatbalthazar@gmail.com>
2026-09-11 12:06:28 +0800

    core structure

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Product API is running!")
+});
+
+// Centralized error handler
+app.use((err, req, res, next) => {
+    return res.status(500).json({
+        error: "Something went wrong on the server...",
+        message: err.message,
+
+    });
+    }
+); 
+
+const PORT = 3000;
+app.listen(PORT, () => {
+        console.log(`Server is running on PORT:${PORT} 🌏`);
+    }
+);
---
+++ server/models/Product.js
@@ -0,0 +1,12 @@
+export const products = [
+    {
+        "name": "Keyboard",
+        "price": 49.99,
+        "quantity": 1
+    },
+    {
+        "name": "Mouse",
+        "price": 25.99,
+        "quantity": 1
+    },
+];
